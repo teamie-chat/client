b3d1732bd71f197ac101ad0404a0204a19554ec8
Amarnath Ravikumar <amar@semantics3.com>
2014-08-17 12:58:02 +0530

    Yet another minor indentation fix.

--- src/scripts/t-chat.directives/t-chat-thread.js
+++ src/scripts/t-chat.directives/t-chat-thread.js
@@ -1,14 +1,14 @@
 'use strict';
 
 angular.module('tChat')
-	.directive('tChatThread', function() {
-		return {
-			restrict: 'E',
-			replace: true,
-			scope: {
-				title: '@'
-			},
-			templateUrl: 'src/partials/directives/t-chat-thread.js',
-			controller: 'ThreadController'
-		}
-	});
+  .directive('tChatThread', function() {
+    return {
+      restrict: 'E',
+      replace: true,
+      scope: {
+        title: '@'
+      },
+      templateUrl: 'src/partials/directives/t-chat-thread.js',
+      controller: 'ThreadController'
+    }
+  });
